7a4d0f402c338307a1e7d23b892611c236614efb
Deven Balaji <devenvbalaji@gmail.com>
2025-07-29 17:06:35 -0400

    added read and write to robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsytems/Robot.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.blucru.common.subsytems;
 
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Robot {
 
@@ -11,6 +15,8 @@ public class Robot {
     //list of subsystems
     ArrayList<BluSubsystem> subsystems;
     static Robot instance;
+    HardwareMap hwMap;
+    List<LynxModule> hubs;
     public static Robot getInstance(){
         if (instance == null){
             instance = new Robot();
@@ -22,6 +28,46 @@ public class Robot {
 
     private Robot(){
         subsystems = new ArrayList<>();
+    }
+
+    public void setHwMap(HardwareMap hwMap){
+        this.hwMap = hwMap;
+    }
+
+    public void init(){
+        //store the hubs in an array
+        hubs = hwMap.getAll(LynxModule.class);
+
+        //set the hubs to auto bulk reads
+        for (LynxModule hub : hubs){
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        //init all subsystems
+        for (BluSubsystem subsystem : subsystems){
+            subsystem.init();
+        }
+    }
+
+    public void read(){
+
+
+        //clear caches of both hubs
+        for (LynxModule hub:hubs){
+            hub.clearBulkCache();
+        }
+
+        //read from each subsytem
+        for (BluSubsystem subsystem: subsystems){
+            subsystem.read();
+        }
+    }
+
+    public void write(){
+        //write to each subsystem
+        for (BluSubsystem subsystem : subsystems){
+            subsystem.write();
+        }
     }
 
 
